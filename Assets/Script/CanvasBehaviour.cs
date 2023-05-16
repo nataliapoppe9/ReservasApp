dc94e2f1714aadcff934c1b9f3344456c7721dbc
Nataliapoppe9 <personaldosis@gmail.com>
2023-05-16 23:29:35 +0200

    Primer Script
    Activar Canvas Nuevo Usuario

--- Assets/Script/CanvasBehaviour.cs
+++ Assets/Script/CanvasBehaviour.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class CanvasBehaviour : MonoBehaviour
 {
+
+    [SerializeField] GameObject StartCanvas;
+    public GameObject NewUserCanvas,SignInCanvas;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCanvas = this.gameObject;
+
+    }
+
+    public void DesactivarObjeto(GameObject canva)
+    {
+        canva.SetActive(false);
+    }
+    public void ActivarObjeto(GameObject canva)
+    {
+        canva.SetActive(true);
     }
 
     // Update is called once per frame
